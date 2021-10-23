4200d53802b81e22ef2000cc90a2dc65ce54e13e
Miguel Nogueira <mglsndc@gmail.com>
2021-10-23 20:00:38 -0300

    fhandle async errors

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,27 @@
 import 'reflect-metadata';
-import './database';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import { router } from './routes';
+import './database';
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.json());
+
 app.use(router);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof Error) {
+    return res.status(400).json({ error: error.message });
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is Running at port ${port}`);
