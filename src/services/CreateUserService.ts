2da24e1cb805405bee8dedaba9d915d6553b494d
Miguel Nogueira <mglsndc@gmail.com>
2021-10-23 18:58:50 -0300

    feat: add create user service

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,37 @@
+import { getCustomRepository } from 'typeorm';
+
+import { UsersRepository } from '../repositories/UsersRepository';
+
+interface IUserRequest {
+  name: string;
+  email: string;
+  admin?: boolean;
+}
+
+class CreateUserService {
+  async execute({ email, name, admin = false }: IUserRequest) {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    if (!email) {
+      throw new Error('Invalid Email.');
+    }
+
+    const userAlreadyExists = await usersRepository.findOne({ email });
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists');
+    }
+
+    const user = usersRepository.create({
+      name,
+      email,
+      admin,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export { CreateUserService };
